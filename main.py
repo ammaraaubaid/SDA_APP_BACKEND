7dac60b6717551b2b56ee59190adc6ac89b5bd65
Ammara-u <ammaraubaid10@gmail.com>
2026-04-25 19:26:15 +0500

    resolved cors issue

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    
+    allow_origins=[
+    "https://sda-front-end-1a92.vercel.app"
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
