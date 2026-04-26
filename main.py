de8427e173e18932c5507c0e7111e3eac805021b
Ammara-u <ammaraubaid10@gmail.com>
2026-04-26 12:22:03 +0500

    resolved post issues

--- main.py
+++ main.py
@@ -25,8 +25,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8081",
+        "http://127.0.0.1:8081",  # ✅ add this
         "http://localhost:8000",
         "https://sda-front-end-xhiu.vercel.app"
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
